3400bb3e1d62654b1fe060e38b506bca7865d9f6
Jack Kuang <Jack.Kuang@ama.ab.ca>
2021-12-16 20:10:58 -0700

    initial push

---
+++ main.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+    # a = np.random(0)
+    a = np.random.rand(2, 3)
+
+    print(a)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
